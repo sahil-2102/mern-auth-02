76c7af69bc7124f374e038ed96f4b568729d0b53
Sahil <sk4386840@gmail.com>
2025-05-24 14:09:15 +0530

    created header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,22 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Profile from './pages/Profile';
+import Header from './components/Header';
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/signin' element={<SignIn/>} />
+        <Route path='/signUp' element={<SignUp/>} />
+        <Route path='/profile' element={<Profile/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <div className='bg-slate-400'>
+        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+            <Link to={'/'}><h1 className='text-3xl font-bold text-blue-950'>Auth App</h1></Link>
+            <ul className='flex justify-center items-center gap-4 '>
+                <Link to={'/'}><li>Home</li></Link>
+                <Link to={'/about'}><li>About</li></Link>
+                <Link to={'signin'}><li>SignIn</li></Link>
+            </ul>
+        </div>
+    </div>
+  )
+}
