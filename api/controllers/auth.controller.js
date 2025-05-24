0166e2cbf2fa527c4db840d21b36739cdbf08b62
Sahil <sk4386840@gmail.com>
2025-05-24 16:12:09 +0530

    created signup api route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const user = new User({ username, email, password: hashedPassword });
+  try {
+    await user.save();
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,14 +2,17 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{console.log("Database connected!")})
 .catch((err)=>{console.log(err)});
 
 const app = express();
+app.use(express.json());
 const port = 3000;
 app.listen(port,()=>{
     console.log(`server running on port : ${port}`)
 });
 
 app.use("/api/user",userRoutes);
+app.use("/api/auth",authRoutes);
