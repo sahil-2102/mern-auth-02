8c329ea807cf76b5280530611fc3e1ea5f272340
Sahil <sk4386840@gmail.com>
2025-05-29 14:53:02 +0530

    added redux and redux persist functionalities

--- api/index.js
+++ api/index.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
-import { error } from 'console';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(()=>{console.log("Database connected!")})
 .catch((err)=>{console.log(err)});
@@ -19,10 +18,10 @@ app.use("/api/user",userRoutes);
 app.use("/api/auth",authRoutes);
 app.use((err,req,res,next) => {
     const statusCode = err.statusCode || 500;
-    const errorMessage = err.message || "Internal server error";
+    const message = err.message || "Internal server error";
     return res.status(statusCode).json({
         success: false,
-        errorMessage,
+        message,
         statusCode,
     });
 })
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,18 +1,19 @@
 import {Link, useNavigate} from "react-router-dom";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice.js";
+import {useDispatch, useSelector} from "react-redux";
 import { useState } from "react";
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [Error, setError] = useState(false);
-  const [loading, setIsLoading] = useState(false);
+  const {loading, error} = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
       setFormData({...formData, [e.target.id]: e.target.value});
   }
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setIsLoading(true);
-      setError(false);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin',{
       method: 'POST',
       headers: {
@@ -21,15 +22,14 @@ export default function SignIn() {
       body: JSON.stringify(formData),
     })
     const data = await res.json();
-    setIsLoading(false);
     if(data.success === false){
-      setError(true);
+      dispatch(signInFailure(data.message));
       return;
     }
+    dispatch(signInSuccess(data));
      navigate('/');
     } catch (error) {
-      setIsLoading(false);
-      setError(true);
+      dispatch(signInFailure(error));
     }
   }
   return (
@@ -49,7 +49,7 @@ export default function SignIn() {
         <Link to={'/signup'}><span className="text-blue-500">Sign Up</span></Link>
       </div>
       <p className="text-red-500 mt-4">
-        {Error && "Something went wrong!"}
+        {error ? error || "Something went wrong": ""}
       </p>
     </div>
   )
